4e1a16e3d128d5155e1450802df86db0891109ca
ayellowman3 <ayellowman3@gmail.com>
2019-12-12 18:18:49 -0500

    scrapying name from table and inserted into csv file

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # draziw_rmg
 
+pip install selenium
+
 pip install selenium pandas
+
+Download chrome driver
+https://chromedriver.chromium.org/home
+
+localpath.py
--- run.py
+++ run.py
@@ -1,22 +1,28 @@
 from selenium import webdriver
 import pandas as pd
+from localPath import *
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\George\Documents\Coding\chromedriver.exe')
+lPath = localPath.path
+
+driver = webdriver.Chrome(executable_path=r'/Users/georgehuang/chromedriver.exe')
 
 url = "https://basketball.realgm.com/nba/draft/prospects/stats"
 
 driver.get(url)
-#//*[@id="table-6081"]/tbody/tr[1]/td[1]
-#//*[@id="table-6081"]/tbody/tr[2]/td[1]
-#//*[@id="table-6081"]/tbody/tr[95]
-items = len(driver.find_elements_by_tag_name("nowrap tablesaw-cell-persist"))
+#<tr><td class='nowrap'>
+#<a href="/player/Markus-Howard/Summary/81690">Markus Howard</a></td>
+#<td>MU</td><td>8</td><td>30.5</td><td>7.1</td><td>17.1</td><td>.416</td><td>3.8</td><td>8.4</td><td>.448</td><td>7.1</td><td>8.4</td><td>.851</td><td>0.2</td><td>2.1</td><td>2.4</td><td>2.8</td><td>0.8</td><td>0.1</td><td>25.1</td></tr>
+
+items = len(driver.find_elements_by_class_name("nowrap"))
 #items = 95
 print(items)
 total = []
+players = driver.find_elements_by_class_name("nowrap")
 for item in range(1, items):
-    players = driver.find_elements_by_id("nowrap tablesaw-cell-persist")
+    print(players[item].text)
     #for player in players:
-    total.append(players)
+    total.append(players[item].text)
+    print(item)
 df = pd.DataFrame(total)
 df.to_csv('players.csv')
 driver.close()
