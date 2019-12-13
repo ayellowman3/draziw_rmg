93cade90ffbee0dd31bb07e71de7ec0755783576
ayellowman3 <ayellowman3@gmail.com>
2019-12-13 01:04:41 -0500

    updated readme for set up. player constructor
    updated readme for set up. player constructor

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
+from player import *
 import pandas as pd
-from localPath import *
 
-lPath = localPath.path
-
-driver = webdriver.Chrome(executable_path=r'/Users/georgehuang/chromedriver.exe')
+driver = webdriver.Chrome(executable_path=r'C:\Users\George\Documents\Coding\chromedriver.exe')
 
 url = "https://basketball.realgm.com/nba/draft/prospects/stats"
 
@@ -14,17 +12,22 @@ driver.get(url)
 #<td>MU</td><td>8</td><td>30.5</td><td>7.1</td><td>17.1</td><td>.416</td><td>3.8</td><td>8.4</td><td>.448</td><td>7.1</td><td>8.4</td><td>.851</td><td>0.2</td><td>2.1</td><td>2.4</td><td>2.8</td><td>0.8</td><td>0.1</td><td>25.1</td></tr>
 
 items = len(driver.find_elements_by_class_name("nowrap"))
+
 print(items)
 
 total = []
 
-players = driver.find_elements_by_class_name("nowrap")
+#players = driver.find_elements_by_class_name("nowrap")
 for item in range(1, items + 1):
     #//*[@id="table-7185"]/tbody/tr[1]/td[1]/a
     #/html/body/div[2]/div[3]/div[1]/table/tbody/tr[1]/td[1]/a
-    player = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/table/tbody/tr['+str(item)+']/td[1]/a')
-    print(player.text)
-    total.append(player.text)
+    name = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/table/tbody/tr['+str(item)+']/td[1]/a')
+    school = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div/table/tbody/tr[1]/td[2]')
+    gp = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div/table/tbody/tr[1]/td[3]')
+    p = player(name, school, gp)
+
+    p.printer()
+    total.append(p)
     #print(players[item].text)
     #for player in players:
     #total.append(players[item].text)
